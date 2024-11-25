eb0dbe942825a8ba6ac22b2a36a3401a7ea05e4e
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-11-25 21:35:23 +0200

    setting up the index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+import App from './App'; // main application component
+import './index.css'; 
+
+const container = document.getElementById('root'); // Get the root element
+const root = ReactDOM.createRoot(container); // Create a root
+
+// Render the application
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
