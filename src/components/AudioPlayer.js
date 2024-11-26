de60fc1653a8d1e6a46d6e0f761051b27279f288
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-11-26 18:53:05 +0200

    Add functionality to play a placeholder audio track for episodes

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,28 +1,26 @@
 import React, { useRef, useState } from 'react';
 
 const AudioPlayer = ({ src }) => {
-  const [playing, setPlaying] = useState(false);
-  const audioRef = useRef(null);
+    const [playing, setPlaying] = useState(false);
+    const audioRef = useRef(null);
 
-  const togglePlay = () => {
-    if (playing) {
-      audioRef.current.pause();
-    } else {
-      audioRef.current.play();
-    }
-    setPlaying(!playing);
-  };
+    const togglePlay = () => {
+        if (playing) {
+            audioRef.current.pause();
+        } else {
+            audioRef.current.play();
+        }
+        setPlaying(!playing);
+    };
 
-  return (
-    <div className="audio-player">
-      <audio ref={audioRef} src={src} />
-      <button onClick={togglePlay}>
-        {playing ? 'Pause' : 'Play'}
-      </button>
-    </div>
-  );
+    return (
+        <div className="audio-player">
+            <audio ref={audioRef} src={src} />
+            <button onClick={togglePlay}>
+                {playing ? 'Pause' : 'Play'}
+            </button>
+        </div>
+    );
 };
 
 export default AudioPlayer;
-
-
